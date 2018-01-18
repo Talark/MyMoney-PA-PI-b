03cf41672a73d159d4ea516cb11ae3c68b53d468
Talar <talar.koushagjian@hotmail.com>
2018-01-18 01:09:58 -0500

    2nd commit develop branch

--- MyMoney/src/Driver.java
+++ MyMoney/src/Driver.java
@@ -3,7 +3,7 @@ public class Driver {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Develop branch first commit.");
+		System.out.println("Develop branch 2nd commit.");
 		
 	}
 
