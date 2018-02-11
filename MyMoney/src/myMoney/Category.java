e78c7124f1841531c163927a713013a40d27bb55
Talark <talar.koushagjian@hotmail.com>
2018-02-10 21:45:18 -0500

    Iteration one: add & list transactions by Sirage

---
+++ MyMoney/src/myMoney/Category.java
@@ -0,0 +1,19 @@
+package myMoney;
+
+public class Category {
+
+	private String catName;
+
+//	================================================================
+	// this is a Constructor
+	public Category(String catName) {
+		this.catName = catName;
+	} 
+//	================================================================
+	// this is an Accessor Method.
+	public String getcatName() {
+		return catName;
+	}
+//	================================================================
+
+}
